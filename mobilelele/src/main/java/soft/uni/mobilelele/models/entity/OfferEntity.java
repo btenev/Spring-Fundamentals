98014455d9f32955fc5263affb51f5c234cf0ce8
Boyan Tenev <104432073+btenev@users.noreply.github.com>
2022-06-02 15:30:45 +0300

    added all entities plus web resources

--- mobilelele/src/main/java/soft/uni/mobilelele/models/entity/OfferEntity.java
+++ mobilelele/src/main/java/soft/uni/mobilelele/models/entity/OfferEntity.java
@@ -1,19 +1,30 @@
 package soft.uni.mobilelele.models.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 import soft.uni.mobilelele.models.enums.EngineEnum;
 import soft.uni.mobilelele.models.enums.TransmissionEnum;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Entity
 @Table(name = "offers")
-public class OfferEntity extends BaseEntity{
+public class OfferEntity  {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+//    @Column(columnDefinition = "uuid-char")
+    @Type(type = "uuid-char")
+    private UUID id;
 
     @Column(columnDefinition = "TEXT")
     private String description;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private EngineEnum engine;
 
     @Column(name = "image_url")
@@ -21,21 +32,127 @@ public class OfferEntity extends BaseEntity{
 
     private int mileage;
 
+    @Column(nullable = false)
     private BigDecimal price;
 
+    @Column(nullable = false)
     private TransmissionEnum transmission;
 
     private int year;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     private ModelEntity model;
 
+    @ManyToOne(optional = false)
     private UserEntity seller;
 //    Create a Model class, which holds the following properties:
 
+    public OfferEntity() {
+    }
 
-//•	created – a date and time.
-//•	modified – a date and time.
-//•	model – the model of a car.
-//•	seller – a user that sells the car.
+    public UUID getId() {
+        return id;
+    }
+
+    public OfferEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public OfferEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public EngineEnum getEngine() {
+        return engine;
+    }
+
+    public OfferEntity setEngine(EngineEnum engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public OfferEntity setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        return this;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public OfferEntity setMileage(int mileage) {
+        this.mileage = mileage;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public OfferEntity setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public TransmissionEnum getTransmission() {
+        return transmission;
+    }
+
+    public OfferEntity setTransmission(TransmissionEnum transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public OfferEntity setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public ModelEntity getModel() {
+        return model;
+    }
+
+    public OfferEntity setModel(ModelEntity model) {
+        this.model = model;
+        return this;
+    }
+
+    public UserEntity getSeller() {
+        return seller;
+    }
+
+    public OfferEntity setSeller(UserEntity seller) {
+        this.seller = seller;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "OfferEntity{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", engine=" + engine +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", mileage=" + mileage +
+                ", price=" + price +
+                ", transmission=" + transmission +
+                ", year=" + year +
+                ", model=" + model +
+                ", seller=" + seller +
+                '}';
+    }
 }
